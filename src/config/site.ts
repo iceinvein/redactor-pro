4d23f846a133c321ad29dd4439b7b94463c329df
Dik Rana <dik.rana@mechanicalrock.io>
2025-10-07 15:19:58 +0545

    init

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "My App",
+  description: "My application built with Vite and HeroUI",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  navMenuItems: [],
+  links: {},
+};
